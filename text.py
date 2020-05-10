bd50dea79d600e23ce7ae5d12f09c1f0da2c7742
lin <584148675@qq.com>
2020-05-10 19:59:15 +0800

    加延时发送

--- text.py
+++ text.py
@@ -1,6 +1,7 @@
 import win32gui
 import win32con
 import win32clipboard as w
+import time
 from win32gui import *
 
 IpClassName="TXGuiFoundation"
@@ -9,9 +10,9 @@ str="我裂开来"
 
 #设置剪贴板文本
 def setText(aString):
-    w.OpenClipboard()
-    w.EmptyClipboard()
-    w.SetClipboardData(win32con.CF_TEXT,aString)
+    w.OpenClipboard()#打开剪贴板
+    w.EmptyClipboard()#清空
+    w.SetClipboardText(aString)# SetClipboardData(,)会乱码，要用SetClipboardText(ste)
     w.CloseClipboard()
 
 #获得剪贴板信息
@@ -24,6 +25,7 @@ def getText():
 win=QQwin = win32gui.FindWindow(IpClassName, IpWindowName)#找到qq窗口
 setText(str)
 win32gui.PostMessage(win,win32con.WM_PASTE,0,0)#黏贴
-# win32gui.SendMessage(getText, 258, 22, 2080193)
-# win32gui.SendMessage(getText, 770, 0, 0)
+time.sleep(1)   # python 中，sleep的参数为秒
+win32gui.SendMessage(win,win32con.WM_KEYDOWN,win32con.VK_RETURN,0)#发送
+
 
